7ee4f0181267d00e9dfc683dcea98cf9a06d7016
arpitma2001 <arpitma161@gmail.com>
2026-04-13 09:04:37 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Demo backend running 🚀");
 });
 
-app.listen(5000, () => console.log("Server started"));
+app.listen(5000, () => {
+  console.log("Server started");
+});
